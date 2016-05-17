5c41abc988b1b6338cc766ba7f6712ecc18f0874
Sriram <sriz.sriram@gmail.com>
2016-05-17 06:03:49 -0500

    updating the server for headers

--- arbor.js
+++ arbor.js
@@ -8,6 +8,22 @@ var debug = require('debug')('arbor');
 var app = express();
 app.use(bodyParser.json());
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 /*
 
 Example request:
