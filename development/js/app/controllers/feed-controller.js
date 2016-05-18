77c767cf288c476f0377c169af5eb21b1b4df173
Salvatore Torcivia <sal@saltor.nyc>
2016-05-18 07:00:36 -0600

    Feed style updates

--- development/js/app/controllers/feed-controller.js
+++ development/js/app/controllers/feed-controller.js
@@ -28,40 +28,28 @@ arbor.controller("feedController", [ "$rootScope", "$scope", "$routeParams", "Li
             .done(function(data) {
                 $scope.feed = [];
                 console.log(data)
+
                 var aggregate = data['data']['twitter'].concat(data.data.instagram);
 
                 aggregate.map(function(index, elem) {
                     index[Object.keys(index)].map(function(index, elem) {
-                        index.create_date_edited = (index['create_date'].match(/ /) == null)
-                            ? Number.parseInt(index['create_date']) : Date.parse(index['create_date']);
-
-                        index.create_date_edited = (index.create_date_edited.toString().length > 10)
-                            ? index.create_date_edited.toString().substr(0, 10) : index.create_date_edited
-
                         $scope.feed = $scope['feed'].concat(index);
                     })
                 })
 
                 $scope.feed = $scope['feed'].sort(function(index, index_1) {
                     return index['create_date'] < index_1['create_date']
-                    console.log()
                 });
 
                 $scope.$digest();
-
-                $scope['feed'].map(function(index, elem) {
-                    // console.log(index)
-                })
             })
             .fail(function()   {})
             .always(function() {});
         }
 
         updateFeed();
-        console.log("called")
 
         setTimeout(function(){
-            console.log("recall");
             updateFeed();
         }, 4000);
     }
