411d22053c6f89a2e4dc9c1998037e96b903fcff
Salvatore Torcivia <sal@saltor.nyc>
2016-05-18 05:13:16 -0600

    Styling galore

--- development/js/app/controllers/feed-controller.js
+++ development/js/app/controllers/feed-controller.js
@@ -10,40 +10,58 @@ arbor.controller("feedController", [ "$rootScope", "$scope", "$routeParams", "Li
             $rootScope.gotoRoute({path: '/'});
         }
 
-        $.ajax({
-            url: 'https://arborapp.herokuapp.com/retrieve/',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(
-                {
-                    "users": [
-                        {"name": "sriram09","source": "twitter"},
-                        {"name": "srirams09","source": "instagram"},
-                        {"name": "saltorcivia","source": "twitter"},
-                    ]
-                }
-            )
-        })
-        .done(function(data) {
-            $scope.feed = [];
+        function updateFeed() {
+            $.ajax({
+                url: 'https://arborapp.herokuapp.com/retrieve/',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(
+                    {
+                        "users": [
+                            {"name": "sriram09","source": "twitter"},
+                            {"name": "srirams09","source": "instagram"},
+                            {"name": "saltorcivia","source": "twitter"},
+                        ]
+                    }
+                )
+            })
+            .done(function(data) {
+                $scope.feed = [];
+console.log(data)
+                var aggregate = data['data']['twitter'].concat(data.data.instagram);
 
-            var aggregate = data['data']['twitter'].concat(data.data.instagram);
+                aggregate.map(function(index, elem) {
+                    index[Object.keys(index)].map(function(index, elem) {
+                        index.create_date_edited = (index['create_date'].match(/ /) == null)
+                            ? Number.parseInt(index['create_date']) : Date.parse(index['create_date']);
 
-            aggregate.map(function(index, elem) {
-                index[Object.keys(index)].map(function(index, elem) {
-                    index.create_date_edited = (index['create_date'].match(/ /) == null) ? Number.parseInt(index['create_date']) : Date.parse(index['create_date']);
-                    $scope.feed = $scope['feed'].concat(index);
+                        index.create_date_edited = (index.create_date_edited.toString().length > 10)
+                            ? index.create_date_edited.toString().substr(0, 10) : index.create_date_edited
+
+                        $scope.feed = $scope['feed'].concat(index);
+                    })
+                })
+
+                $scope.feed = $scope['feed'].sort(function(index, index_1) {
+                    return index['create_date_edited'] < index_1['create_date_edited']
+                });
+
+                $scope.$digest();
+
+                $scope['feed'].map(function(index, elem) {
+                    // console.log(index)
                 })
             })
+            .fail(function()   {})
+            .always(function() {});
+        }
 
-            $scope['feed'].sort(function(index, index_1) {
-                return index['create_date_edited'] > index_1['create_date_edited']
-            });
+        updateFeed();
+        console.log("called")
 
-            $scope.$digest();
-            console.log($scope.feed)
-        })
-        .fail(function()   {})
-        .always(function() {});
+        setTimeout(function(){
+            console.log("recall");
+            updateFeed();
+        }, 4000);
     }
 ]);
